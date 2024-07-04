9d2dbd8d292d8224765a0b8a17123e2a0b4c66c6
JayaSanthosh7066 <jayasanthoshamalakanti@gmail.com>
2024-07-04 15:34:21 +0530

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 import React from "react";
